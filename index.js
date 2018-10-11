b61b4578de5f20fb7946dcff1b177c72932e95a3
UpDownLeftDie <updownleftdie@KOTOFF.local>
2018-10-10 22:58:03 -0700

    Get all files. Filter files by type. Get metadata of video files

--- index.js
+++ index.js
@@ -1,28 +1,69 @@
 const _ = require('lodash');
 const fs = require('fs');
 const spawnSync = require('child_process').spawnSync;
+const path = require('path');
+const ffmpeg = require('fluent-ffmpeg');
 
-const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const FILE_FORMATS = [".mp4", ".flv", ".mkv"];v
+
+const config = loadConfig();
+
+function loadConfig() {
+    let config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+    config.fileFormats = FILE_FORMATS;
+    if (config.ignoreFileFormats && config.ignoreFileFormats.length) {
+        config.ignoreFileFormats.forEach(ignoredFileFormat => {
+            const index = config.fileFormats.indexOf(ignoredFileFormat.toLowerCase());
+            if (index !== -1) {
+                config.fileFormats.splice(index, 1);
+            }
+        });
+    }
+    return config;
+}
 
 main();
 
-function main() {
-    cleanUp();
+async function main() {
+    const { inputFolder, fileFormats } = config;
+    const files = await getAllFiles(inputFolder, fileFormats);
 }
 
-function cleanUp(){
-    
-    deleteFile();
+async function getAllFiles(dir, fileFormats) {
+    let files = fs.readdirSync(dir);
+    files = await files.reduce(async (previousPromise, file) => {
+        let filterdFiles = await previousPromise;
+        const index = fileFormats.indexOf(path.extname(file).toLowerCase());
+        if (index !== -1) {
+            const metadata = await readVideoFile(path.join(dir, file));
+            filterdFiles[file] = metadata;
+        }
+        return filterdFiles;
+    }, Promise.resolve({}));
+
+    return files;
 }
 
-function getAllFiles() {
-
+function readVideoFile(filePath) {
+    return new Promise((resolve, reject) => {
+        ffmpeg.ffprobe(filePath, function(err, metadata) {
+            if (err) reject(err);
+            resolve(metadata);
+        });
+    });
 }
 
-function deleteFile() {
+// function cleanUp(){
 
-}
+//     deleteFile();
+// }
 
-function createFFmpegProcess() {
 
-}
+
+// function deleteFile() {
+
+// }
+
+// function createFFmpegProcess() {
+
+// }
